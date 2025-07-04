ca21a0d549263a54639ffc86f49c471edaa67d09
vinhmax <vinhmax200@gmail.com>
2025-07-04 10:09:17 +0700

    Update ALU_decoder.v

--- ALU_decoder.v
+++ ALU_decoder.v
@@ -1,20 +1,24 @@
 module ALU_decoder(
-    input      [6:0] funct7,
-    input      [2:0] funct3,
-    input            ALUSrc,
-    output reg [3:0] ALUOp
+    input  [1:0] alu_op,
+    input  [2:0] funct3,
+    input        funct7b5,
+    output logic [3:0] alu_control
 );
-    always @(*) begin
-        case (funct3)
-            3'b000: ALUOp = (funct7 == 7'b0100000) ? 4'b0001 : 4'b0000; // SUB or ADD
-            3'b111: ALUOp = 4'b0010; // AND
-            3'b110: ALUOp = 4'b0011; // OR
-            3'b100: ALUOp = 4'b0100; // XOR
-            3'b001: ALUOp = 4'b0101; // SLL
-            3'b101: ALUOp = (funct7 == 7'b0000000) ? 4'b0110 : 4'b0111; // SRL or SRA
-            3'b010: ALUOp = 4'b1000; // SLT
-            3'b011: ALUOp = 4'b1001; // SLTU
-            default: ALUOp = 4'b0000;
+    always_comb begin
+        alu_control = 4'b0010; // ADD mặc định (load/store)
+        case (alu_op)
+            2'b00: alu_control = 4'b0010; // ADD
+            2'b01: alu_control = 4'b0110; // SUB (branch)
+            2'b10: begin // R-type/I-type
+                case (funct3)
+                    3'b000: alu_control = (funct7b5 ? 4'b0110 : 4'b0010); // SUB : ADD
+                    3'b111: alu_control = 4'b0000; // AND
+                    3'b110: alu_control = 4'b0001; // OR
+                    3'b010: alu_control = 4'b0111; // SLT
+                    default: alu_control = 4'b0010;
+                endcase
+            end
+            default: alu_control = 4'b0010;
         endcase
     end
 endmodule
