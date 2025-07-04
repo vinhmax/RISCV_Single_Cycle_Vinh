7938eae8a81e85db821a6d31320566549a22388f
vinhmax <vinhmax200@gmail.com>
2025-07-04 10:10:55 +0700

    Update DMEM.v

--- DMEM.v
+++ DMEM.v
@@ -1,15 +1,29 @@
-module DMEM(
-    input         clk,
-    input  [31:0] addr,
-    input  [31:0] wdata,
-    input         we,
-    output [31:0] rdata
+module DMEM (
+    input logic clk,
+    input logic rst_n,
+    input logic MemRead,
+    input logic MemWrite,
+    input logic [31:0] addr,
+    input logic [31:0] WriteData,
+    output logic [31:0] ReadData
 );
-    reg [31:0] memory [0:255];  // <-- Tên phải là memory
+    logic [31:0] memory [0:255];
 
-    always @(posedge clk) begin
-        if (we)
-            memory[addr[9:2]] <= wdata;
+    assign ReadData = (MemRead) ? memory[addr[9:2]] : 32'b0;
+
+    always_ff @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            for (int i = 0; i < 256; i = i + 1)
+                memory[i] <= 32'b0;
+        end else if (MemWrite) begin
+            memory[addr[9:2]] <= WriteData;
+        end
     end
-    assign rdata = memory[addr[9:2]];
+
+    initial begin
+        if ($fopen("./mem/dmem_init2.hex", "r"))
+            $readmemh("./mem/dmem_init2.hex", memory);
+        else if ($fopen("./mem/dmem_init.hex", "r"))
+            $readmemh("./mem/dmem_init.hex", memory);
+    end
 endmodule
