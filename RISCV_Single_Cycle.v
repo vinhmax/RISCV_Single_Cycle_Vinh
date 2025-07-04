e70b7f529717d13926e2499d3386a03278097c3d
vinhmax <vinhmax200@gmail.com>
2025-07-04 09:38:00 +0700

    Update RISCV_Single_Cycle.v

--- RISCV_Single_Cycle.v
+++ RISCV_Single_Cycle.v
@@ -1,6 +1,6 @@
 module RISCV_Single_Cycle(
     input         clk,
-    input         rst
+    input         rst_n
 );
 // Đây là module top, bạn sẽ tự kết nối các module nhỏ ở trên theo sơ đồ single-cycle CPU.
 // Do mỗi lab, mỗi thầy sẽ có sơ đồ và tín hiệu riêng, dưới đây chỉ là ví dụ tối giản.
