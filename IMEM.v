0ba374a09a6780bcc9f2486cf61bc495c195ce81
vinhmax <vinhmax200@gmail.com>
2025-07-04 09:59:49 +0700

    Update IMEM.v

--- IMEM.v
+++ IMEM.v
@@ -2,7 +2,9 @@ module IMEM(
     input  [31:0] addr,
     output [31:0] inst
 );
-    reg [31:0] mem [0:255];
-    initial $readmemh("imem.txt", mem);
-    assign inst = mem[addr[9:2]];
+    reg [31:0] memory [0:255];  // <-- Tên phải là memory
+
+    initial $readmemh("imem.txt", memory);
+
+    assign inst = memory[addr[9:2]];
 endmodule
