e4d86baef907f69e78857a4b8ce555d502d1eec7
vinhmax <vinhmax200@gmail.com>
2025-07-04 10:11:15 +0700

    Update IMEM.v

--- IMEM.v
+++ IMEM.v
@@ -1,10 +1,15 @@
-module IMEM(
+module IMEM (
     input  [31:0] addr,
-    output [31:0] inst
+    output [31:0] Instruction
 );
-    reg [31:0] memory [0:255];  // <-- Tên phải là memory
+    reg [31:0] memory [0:255];
+    // Trả về lệnh halt (32'h00000063) nếu truy cập vượt quá 128 lệnh (dành cho SC1), vẫn giữ đủ 256 dòng cho SC2
+    assign Instruction = (addr[11:2] < 128) ? memory[addr[11:2]] : 32'h00000063; // halt = beq x0, x0, 0
 
-    initial $readmemh("imem.txt", memory);
-
-    assign inst = memory[addr[9:2]];
+    initial begin
+        if ($fopen("./mem/imem2.hex", "r"))
+            $readmemh("./mem/imem2.hex", memory);
+        else if ($fopen("./mem/imem.hex", "r"))
+            $readmemh("./mem/imem.hex", memory);
+    end
 endmodule
